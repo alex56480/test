5d2a267bf3f8df3b2063de16474032a706315a7a
alex56480 <atorres@calistolabs.com>
2011-10-14 16:19:33 -0700

    fsdfdffad

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $output = shell_exec('git help');
 echo "<pre>$output</pre>";
 
-if(is_dir("C:\Program Files (x86)\Git\bin")){
+if(is_dir("C:\Program Files (x86)\Git\bin  ")){
 	echo "Is DIR!!  ";
 }else{
 	echo "Is not dir!!  ";
